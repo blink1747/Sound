b891d72ba4996cb9ba4cad32da98057d87eaabc6
Sujeet Poudel <sujeetpoudel.np@gmail.com>
2019-08-08 14:53:40 -0500

    With Next and Back Buttons

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -13,7 +13,30 @@ class ViewController: UIViewController {
     
     var player = AVAudioPlayer()
     
+    let songsName = ["AgeOfWorry", "LeavingOnTheJetPlane", "HeyJude", "Bimbaakash4" , "Prayas5", "Aaudai6", "Syndicate7", "Gede8", "Baal9", "ApnaTime10"]
+    
+    let songsCount = 10
+    
+    var currentSong = 0
+    
     @IBOutlet weak var volumeSlider: UISlider!
+    
+    @IBAction func Next(_ sender: Any) {
+        currentSong += 1
+        if currentSong>songsCount-1 {currentSong = songsCount-1}
+        playSong(currentSong)
+    }
+    
+    @IBAction func Back(_ sender: Any) {
+        currentSong -= 1
+              print(currentSong)
+        if currentSong<0 {
+            
+            currentSong = 0
+            
+        }
+        playSong(currentSong)
+    }
     
     @IBAction func play(_ sender: Any) {
         player.play()
@@ -24,14 +47,56 @@ class ViewController: UIViewController {
     @IBAction func volumeSliderAction(_ sender: Any) {
         player.volume = volumeSlider.value
     }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if event?.subtype == UIEvent.EventSubtype.motionShake {
+            print("Device was shaken.")
+            
+             playSong(Int.random(in: 0 ... songsCount))
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let audioPath = Bundle.main.path(forResource: "AgeOfWorry", ofType: "mp3")
+        
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.swipped(gesture:)))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.swipped(gesture:)))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        playSong(0)
+        
+    }
+    
+    func playSong(_ currentSong:Int) {
+        
+        print(currentSong)
+        let audioPath = Bundle.main.path(forResource: songsName[currentSong], ofType: "mp3")
         do {
             player = try AVAudioPlayer (contentsOf: URL(fileURLWithPath: audioPath!))
         } catch {
             print("Error Page")
+        }
+        
+        player.play()
+    }
+    
+    
+    @objc func swipped(gesture: UISwipeGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            if swipeGesture.direction == UISwipeGestureRecognizer.Direction.right {
+                Back(currentSong)
+                
+            } else {
+                Next(currentSong)
+            }
         }
     }
 
