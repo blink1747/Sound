6e8a8fae91a4207c40f9072f33397eea180ee3be
Sujeet Poudel <sujeetpoudel.np@gmail.com>
2019-08-08 11:27:37 -0500

    Final

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player = AVAudioPlayer()
+    
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    @IBAction func play(_ sender: Any) {
+        player.play()
+    }
+    @IBAction func pauseAction(_ sender: Any) {
+        player.pause()
+    }
+    @IBAction func volumeSliderAction(_ sender: Any) {
+        player.volume = volumeSlider.value
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let audioPath = Bundle.main.path(forResource: "AgeOfWorry", ofType: "mp3")
+        do {
+            player = try AVAudioPlayer (contentsOf: URL(fileURLWithPath: audioPath!))
+        } catch {
+            print("Error Page")
+        }
     }
 
 
